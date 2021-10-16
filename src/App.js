668415fbff34f198caa08202735b6cc8c3af440c
Matheus Henrique <matheushenriquedasilvaa.2021@gmail.com>
2021-10-16 18:49:20 -0300

    user styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Cadastro from './components/Cadastro/Cadastro';
+import Teste from './components/teste/Teste';
 
 function App() {
   return (
     <div className="App">
       <Cadastro />
+      <Teste />
     </div>
   );
 }
---
+++ src/components/teste/Teste.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './Teste';
+
+function teste() {
+  return(
+      <Container>
+          hello
+      </Container>
+  );
+}
+
+export default teste;
