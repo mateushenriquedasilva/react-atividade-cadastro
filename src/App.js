ee701ecc8dbf57e81d85a2e787b378a968b15744
Matheus Henrique <matheushenriquedasilvaa.2021@gmail.com>
2021-10-16 19:04:37 -0300

    project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Cadastro from './components/Cadastro/Cadastro';
-import Teste from './components/teste/Teste';
 
 function App() {
   return (
     <div className="App">
       <Cadastro />
-      <Teste />
     </div>
   );
 }
